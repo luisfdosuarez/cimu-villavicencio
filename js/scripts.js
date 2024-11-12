7da851cacaa012db4ad3661fee446762baa5ef39
luisfdosuarez <luisfsuav@gmail.com>
2024-11-12 16:54:59 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener('scroll', function () {
     const welcomeText = document.getElementById('welcomeText');
     const scrollPosition = window.scrollY;
 
-    // Muestra el texto cuando el usuario hace scroll a 200px
     if (scrollPosition > 200) {
         welcomeText.classList.add('visible');
     } else {
