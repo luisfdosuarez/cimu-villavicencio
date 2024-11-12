aff09b5600079853ba978f307c5c60765d387132
luisfdosuarez <luisfsuav@gmail.com>
2024-11-12 17:02:00 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('scroll', function () {
     const welcomeText = document.getElementById('welcomeText');
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > 200) {
+    if (scrollPosition > 100) {
         welcomeText.classList.add('visible');
     } else {
         welcomeText.classList.remove('visible');
