e33a21ade48fbb59b7c011d2db93bc78c4ae89a2
luisfdosuarez <luisfsuav@gmail.com>
2024-11-12 14:48:45 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+// scripts.js
 
+document.addEventListener('scroll', function() {
+    const parallaxContainer = document.querySelector('.parallax-container');
+    const scrollPosition = window.scrollY;
+
+    // Cambia la imagen de fondo después de 300 píxeles de desplazamiento
+    if (scrollPosition > 300) {
+        parallaxContainer.style.backgroundImage = "url('../img/CIMU_NO_RIDGES.png')";
+    } else {
+        parallaxContainer.style.backgroundImage = "url('../img/CIMU_COIN.jpg')";
+    }
+});
