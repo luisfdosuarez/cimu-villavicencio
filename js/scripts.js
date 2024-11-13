1962e3e7b28362e21ea0d258677f818042fbab63
luisfdosuarez <luisfsuav@gmail.com>
2024-11-13 11:41:58 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -83,8 +83,6 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('scroll', function () {
     const video1 = document.getElementById('backgroundVideo1');
     const text1 = document.getElementById('scrollText1');
-    const video2 = document.getElementById('backgroundVideo2');
-    const text2 = document.getElementById('scrollText2');
     const scrollPosition = window.scrollY;
 
     // Mostrar el primer video y texto cuando se hace scroll entre 100 y 800px
@@ -98,18 +96,5 @@ document.addEventListener('scroll', function () {
         video1.pause();
         text1.style.opacity = 0; // Oculta el texto fuera de rango
         text1.style.transform = 'translateY(0)'; // Restablece la posición del texto
-    }
-
-    // Mostrar el segundo video y texto cuando se hace scroll entre 900 y 1600px
-    if (scrollPosition >= 900 && scrollPosition < 1600) {
-        video2.style.opacity = 1; // Muestra el segundo video
-        video2.play(); // Reproduce el segundo video
-        text2.style.opacity = (scrollPosition - 900) / 700; // Incrementa opacidad del texto
-        text2.style.transform = `translateY(-${(scrollPosition - 900) / 10}%)`; // Desplaza el texto hacia arriba
-    } else {
-        video2.style.opacity = 0; // Oculta el segundo video fuera de rango
-        video2.pause();
-        text2.style.opacity = 0; // Oculta el texto fuera de rango
-        text2.style.transform = 'translateY(0)'; // Restablece la posición del texto
     }
 });
